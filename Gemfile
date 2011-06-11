65ac54a83331dcdd9b98f4df1defff725e74ecdd
Steve Faulkner <southpolesteve@gmail.com>
2011-06-11 12:41:16 -0500

    Added all files

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
+
+# Asset template engines
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratch::Application.config.secret_token = '8f2feffaf93eb6d81ba969128cc94c08d66a798b649b5ca3ff090db53c52aa776f8921d692568fc26770b8f43dcb1bc310ac32d4d4f51158f75c7f9917d98a08'
